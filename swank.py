f0549c35b70c7d28bb765d64bd328e35e22e72d2
Samium Gromoff <deepfire@betelheise.deep>
2011-10-11 23:02:43 +0400

    presentations

--- swank.py
+++ swank.py
@@ -1408,3 +1408,200 @@ def emacs_inspect(object):
 #   c(list(paste("a ", class(thing)[[1]], sep=""), list(quote(`:newline`))))
 # }
                 return ["a %s" % type(object).__name__, intern(":newline")]
+
+###
+### Presentations
+###
+record_repl_results = True
+object_to_presentation_id = dict()
+presentation_id_to_object = dict()
+
+def clear_presentation_tables():
+        object_to_presentation_id = dict()
+        presentation_id_to_object = dict()
+
+presentation_counter = 0
+
+nil_surrogate = intern("nil_surrogate")
+
+def save_presented_object(object):
+        object = (nil_surrogate if nonep(object) else object,)
+        def store():
+                global presentation_counter
+                presentation_counter += 1
+                object_to_presentation_id[object] = presentation_counter
+                presentation_id_to_object[presentation_counter] = object
+        return (object_to_presentation_id.get(object) or
+                store())
+
+def lookup_presented_object(id):
+        if integerp(id):
+                (object,), foundp = gethash(id, presentation_id_to_object)
+                if object is nil_surrogate:
+                        return False, True
+                else:
+                        return object, foundp
+        elif listp(id):
+                if id[0] == intern(":frame-var"):
+                        thread_id, frame, index = id[1:]
+                        handler_case(lambda: frame_var_value(frame, index),
+                                     error = lambda: None, None,
+                                     no_error = lambda value: value, True)
+                elif id[0] == intern(":inspected-part"):
+                        part_index, inspectee_parts = id[1], env.inspectee_parts
+                        if part_index < len(inspectee_parts):
+                                return None, None
+                        else:
+                                return inspector_nth_part(part_index), True
+                else:
+                        error("Bad presented object ID: %s", id)
+
+def lookup_presented_object_or_lose(id):
+        object, foundp = lookup_presented_object_or_lose(id)
+        return object if foundp else error("Attempt to access unrecorded object (id %s).", id)
+
+def clear_repl_results():
+        clear_presentation_tables()
+
+def present_repl_results(values):
+        def send(value):
+                id = record_repl_results and save_presented_object(value)
+                send_to_emacs([intern(":presentation-start"), id,                     intern(":repl-result")])
+                send_to_emacs([intern(":write-string"),       prin1_to_string(value), intern(":repl-result")])
+                send_to_emacs([intern(":presentation-end"),   id,                     intern(":repl-result")])
+                send_to_emacs([intern(":write-string"),       "\n",                   intern(":repl-result")])
+        printf("\n")
+        if not values:
+                send_to_emacs([intern(":write-string"),       "; No value.",          intern(":repl-result")])
+        else:
+                mapc(send, values)
+
+#### Presentation menu protocol
+##
+## To define a menu for a type of object, define a method
+## menu-choices-for-presentation on that object type.  This function
+## should return a list of two element lists where the first element is
+## the name of the menu action and the second is a function that will be
+## called if the menu is chosen. The function will be called with 3
+## arguments:
+##
+## choice: The string naming the action from above
+##
+## object: The object 
+##
+## id: The presentation id of the object
+##
+## You might want append (when (next-method-p) (call-next-method)) to
+## pick up the Menu actions of superclasses.
+##
+
+presentation_active_menu = None
+
+def menu_choices_for_presentation_id(id):
+        global presentation_active_menu
+        ob, presentp = lookup_presented_object(id)
+        if not presentp:
+                return intern("not-present")
+        else:
+                menu_and_actions = menu_choices_for_presentation(ob)
+                presentation_active_menu = [id] + menu_and_actions
+                return mapcar(first, menu_and_actions)
+
+# (defun swank-ioify (thing)
+#   (cond ((keywordp thing) thing)
+# 	((and (symbolp thing)(not (find #\: (symbol-name thing))))
+# 	 (intern (symbol-name thing) 'swank-io-package))
+# 	((consp thing) (cons (swank-ioify (car thing)) (swank-ioify (cdr thing))))
+# 	(t thing)))
+def swank_ioify(thing):
+        if keywordp(thing):
+                return thing
+        elif symbolp(thing) and not find(":", symbol_name(thing)):
+                return intern()
+
+(defun execute-menu-choice-for-presentation-id (id count item)
+  (let ((ob (lookup-presented-object id)))
+    (assert (equal id (car *presentation-active-menu*)) () 
+	    "Bug: Execute menu call for id ~a  but menu has id ~a"
+	    id (car *presentation-active-menu*))
+    (let ((action (second (nth (1- count) (cdr *presentation-active-menu*)))))
+      (swank-ioify (funcall action item ob id)))))
+
+
+(defgeneric menu-choices-for-presentation (object)
+  (:method (ob) (declare (ignore ob)) nil)) ; default method
+
+;; Pathname
+(defmethod menu-choices-for-presentation ((ob pathname))
+  (let* ((file-exists (ignore-errors (probe-file ob)))
+	 (lisp-type (make-pathname :type "lisp"))
+	 (source-file (and (not (member (pathname-type ob) '("lisp" "cl") :test 'equal))
+			   (let ((source (merge-pathnames lisp-type ob)))
+			     (and (ignore-errors (probe-file source))
+				  source))))
+	 (fasl-file (and file-exists 
+			 (equal (ignore-errors
+				  (namestring
+				   (truename
+				    (compile-file-pathname
+				     (merge-pathnames lisp-type ob)))))
+				(namestring (truename ob))))))
+    (remove nil 
+	    (list*
+	     (and (and file-exists (not fasl-file))
+		  (list "Edit this file" 
+			(lambda(choice object id) 
+			  (declare (ignore choice id))
+			  (ed-in-emacs (namestring (truename object)))
+			  nil)))
+	     (and file-exists
+		  (list "Dired containing directory"
+			(lambda (choice object id)
+			  (declare (ignore choice id))
+			  (ed-in-emacs (namestring 
+					(truename
+					 (merge-pathnames
+					  (make-pathname :name "" :type "") object))))
+			  nil)))
+	     (and fasl-file
+		  (list "Load this fasl file"
+			(lambda (choice object id)
+			  (declare (ignore choice id object)) 
+			  (load ob)
+			  nil)))
+	     (and fasl-file
+		  (list "Delete this fasl file"
+			(lambda (choice object id)
+			  (declare (ignore choice id object)) 
+			  (let ((nt (namestring (truename ob))))
+			    (when (y-or-n-p-in-emacs "Delete ~a? " nt)
+			      (delete-file nt)))
+			  nil)))
+	     (and source-file 
+		  (list "Edit lisp source file" 
+			(lambda (choice object id) 
+			  (declare (ignore choice id object)) 
+			  (ed-in-emacs (namestring (truename source-file)))
+			  nil)))
+	     (and source-file 
+		  (list "Load lisp source file" 
+			(lambda(choice object id) 
+			  (declare (ignore choice id object)) 
+			  (load source-file)
+			  nil)))
+	     (and (next-method-p) (call-next-method))))))
+
+(defmethod menu-choices-for-presentation ((ob function))
+  (list (list "Disassemble"
+              (lambda (choice object id) 
+                (declare (ignore choice id)) 
+                (disassemble object)))))
+
+(defslimefun inspect-presentation (id reset-p)
+  (let ((what (lookup-presented-object-or-lose id)))
+    (when reset-p
+      (reset-inspector))
+    (inspect-object what)))
+
+
+(setq *send-repl-results-function* 'present-repl-results)
