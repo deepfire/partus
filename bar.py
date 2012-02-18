098b02be09653a5c126ed1020e3964b333a99318
Samium Gromoff <deepfire@gemini>
2012-02-18 13:10:14 +0400

    matcher: CL-ification, in preparation for..

--- bar.py
+++ bar.py
@@ -1,67 +1,70 @@
-name = "name"
+import sys as _sys
 
-## attic?
-def merg(d1:dict, d2:dict):
-        cross = set(d1.keys()) & set(d2.keys())
-        if cross: raise Exception("Binding conflict on names %s." % cross)
-        r = dict(d1)
-        r.update(d2)
-        return r
-
-## debug
-depth = 0
-def deeper(f):
-        global depth
-        try:
-                depth += 1
-                return f()
-        finally:
-                depth -= 1
-def dprint(ctl, *args):
-        print(" " * (depth * 2) + (ctl % args))
+def _debug_printf(format_control, *format_args):
+        print((format_control % format_args + "\n"), file = _sys.stderr)
 
 ## utility part
 def identity(x):    return x
 def integerp(x):    return isinstance(x, int)
 def listp(x):       return isinstance(x, tuple)
 def atom(x):        return not isinstance(x, tuple)
-def cut(n, xs):     return xs[0:n], xs[len(xs) if n is None else n:]
 def position(x, xs):
         for i, ix in enumerate(xs):
                 if x == ix: return i
-def undict_val(xs): return tuple(xs.items())[0][1]
+def typep(x, type): return isinstance(x, type)
+def length(x):      return len(x)
+
+def _tuplep(x):     return isinstance(x, tuple)
+
+def _python_builtins_dictionary():
+        import builtins    as _builtins
+        return _builtins.getattr(__builtins__, "__dict__", __builtins__)
+
+import collections as _collections
+
+class _dictator(_collections.UserDict):
+        def __hasattr__(self, name): return name in self.data
+        def __getattr__(self, name): return self.data[name]
+        def __setitem__(self, *_):   raise  self.data["Exception"]("Dictator.")
+        def __delitem__(self, *_):   raise  self.data["Exception"]("Dictator.")
+        def __setattr__(self, *_):   raise  self.data["Exception"]("Dictator.")
+        def __init__(self, dict):
+                self.__dict__.update(data = dict)
+
+_py = _dictator(_python_builtins_dictionary())
 
 ## A large part of work is development of a calling convention.
 ## Multiple values, as a concept, is an important, but basic step
 ## in the general direction.
-                
+
 ## generic part
-some     = "some"
-maybe    = "maybe"
-_error   = "error"
-_replace = "replace"
-def bind(value, bound, name, if_exists:{_error, _replace} = _error) -> dict:
+some      = "some"
+error     = "error"
+replace   = "replace"
+_name     = "name"
+_maybe    = "maybe"
+def _error_bad_pattern(pat):
+        raise Exception("Bad pattern: %s." % (pat,))
+def _maybe_destructure_binding(pat):
+        return ((None, pat)           if not typep(pat, _py.dict) else
+                tuple(pat.items())[0] if length(pat) == 1         else
+                error_bad_pattern(pat))
+def bind(value, bound, name, if_exists:{error, replace} = error) -> dict:
         def error_bound(x):
-                raise Exception("Rebinding %s from %s to %s." % (repr(name), repr(x), repr(value)))
+                raise Exception("Rebinding %s from %s to %s." % (_py.repr(name), _py.repr(x), _py.repr(value)))
         def error_keyword(name, bad, allowed):
-                raise Exception("Keyword %s must be one of: %s.  Was: %s." % (name, allowed, bad))
+                raise Exception("Keyword %s must be one of: %s.  Was: %s." % (_py.repr(name), allowed, _py.repr(bad)))
         if   name is None:            return bound
         elif (name not in bound or
-              if_exists is _replace):
-                bound = dict(bound)
+              if_exists is replace):
+                bound = _py.dict(bound)
                 bound.update({name:value}); return bound
-        elif if_exists is _error:     error_bound(bound[name])
-        else:                         error_keyword("if_exists", if_exists, { _error, _replace })
-def error_bad_pattern(pat):
-        raise Exception("Bad pattern: %s." % (pat,))
-def maybe_destructure_binding(pat):
-        return ((None, pat)           if not isinstance(pat, dict) else
-                tuple(pat.items())[0] if len(pat) == 1             else
-                error_bad_pattern(pat))
+        elif if_exists is error:     error_bound(bound[name])
+        else:                        error_keyword("if_exists", if_exists, { error, replace })
 def succ(bound, res):      return bound, res, None
-def post(x, mutator):      return (x[0], mutator(x[1]), None) if x[2] is None else x
 def fail(bound, exp, pat): return bound, exp, pat
-def test(test, bound, name, resf:"() -> result", exp, fail_pat, if_exists:{_error, _replace} = _error):
+def post(x, mutator):      return (x[0], mutator(x[1]), None) if x[2] is None else x
+def test(test, bound, name, resf:"() -> result", exp, fail_pat, if_exists:{error, replace} = error):
         return (succ(bind(exp, bound, name, if_exists = if_exists), resf()) if test else
                 fail(bound, exp, fail_pat))
 def equo(name, exp, x):
@@ -90,33 +93,39 @@ def crec(l0, lR, leader = False):
         return (succ(boundR, result) if failpat is None else
                 fail(boundR or bound0, failex, failpat))
 
-__complex_patterns__ = dict()
+def _matcher_init():
+        global __complex_patterns__
+        __complex_patterns__ = dict()
+        register_complex_matcher(some, segment_match)
+        register_complex_matcher(_maybe, match_maybe)
+
 def register_complex_matcher(name, matcher):
         __complex_patterns__[name] = matcher
 def complex_pat_p(x):
         return x and isinstance(x[0], str) and x[0] in __complex_patterns__
 def match_complex(bound, name, exp, pat, leader, aux, limit):
-        # dprint("match_complex  %20s  %10s  %s  %s  %s  %s  %s", bound, name, exp, pat, leader, aux, limit)
+        # _debug_printf("match_complex  %20s  %10s  %s  %s  %s  %s  %s", bound, name, exp, pat, leader, aux, limit)
         return __complex_patterns__[pat[0][0]](bound, name, exp, pat, leader, aux, limit)
-def matcher_not_implemented(bound, name, exp, pat, leader, aux):
-        raise Exception("Not yet capable of matching complex patterns of type %s.", pat[0][0])
 def identity_matcher(bound, name, exp, pat, leader, aux, limit):
         ## This should dispatch over simplicity.
         return match_complex(bound, name, exp, pat[0][1:] + pat[1:], leader, aux, limit)
-
+def matcher_not_implemented(bound, name, exp, pat, leader, aux):
+        raise Exception("Not yet capable of matching complex patterns of type %s.", pat[0][0])
+###
 def segment_match(bound, name, exp, pat, leader, aux, limit, end = None):
+        def cut(n, xs):     return xs[0:n], xs[len(xs) if n is None else n:]
         def constant_pat_p(pat):
                 def nonconstant_pat_p(x): return listp(x) or nonliteral_atom_p(x)
-                return not nonconstant_pat_p(undict_val(pat) if isinstance(pat, dict) else
+                return not nonconstant_pat_p(tuple(pat.items())[0][1] if isinstance(pat, dict) else
                                              pat)
-        # dprint("segment_match  %20s  %10s  %s  %s  %s  %s  %s", bound, name, exp, pat, leader, aux, limit)
+        # _debug_printf("segment_match  %20s  %10s  %s  %s  %s  %s  %s", bound, name, exp, pat, leader, aux, limit)
         ## Unregistered Issue PYTHON-DESTRUCTURING-WORSE-THAN-USELESS-DUE-TO-NEEDLESS-COERCION
         seg_pat, rest_pat = pat[0][1:], pat[1:]
         end = (end                        if end is not None                          else
                position(rest_pat[0], exp) if rest_pat and constant_pat_p(rest_pat[0]) else
                0)
-        if ((end and end > len(exp)) or ## no boundary variant fitted
-            end is None):               ## a constant pattern was missing
+        if ((end and end > length(exp)) or ## no boundary variant fitted
+            end is None):                  ## a constant pattern was missing
                 return fail(bound, exp, pat)
         seg_exp, rest_exp = (cut(end, exp) if rest_pat else
                              (exp, ()))
@@ -124,7 +133,7 @@ def segment_match(bound, name, exp, pat, leader, aux, limit, end = None):
         return coor(crec(lambda:
                                  ((lambda seg_bound, seg_r, seg_fail_pat:
                                            test(seg_fail_pat is None, seg_bound, name, (lambda: seg_r), seg_exp, seg_fail_pat,
-                                                if_exists = _replace))
+                                                if_exists = replace))
                                   (*(  succ(bind((), bound, name), prod((), False)) if seg_exp == () else
                                        fail(bound, exp, pat)                        if limit == 0    else
                                        ## Try biting one more iteration off seg_exp:
@@ -137,12 +146,8 @@ def segment_match(bound, name, exp, pat, leader, aux, limit, end = None):
                                             end + 1),
                     leader = leader)
 
-register_complex_matcher(some, segment_match)
-
 def match_maybe(bound, name, exp, pat, leader, aux, limit):
         return segment_match(bound, name, exp, ((some,) + pat[0][1:],) + pat[1:], leader, aux, 1)
-
-register_complex_matcher(maybe, match_maybe)
 
 ## About the vzy33c0's idea:
 ## type-driven variable naming is not good enough, because:
@@ -151,14 +156,14 @@ register_complex_matcher(maybe, match_maybe)
 def _match(bound, name, exp, pat, leader, aux, limit):
         def maybe_get0Rname(pat):
                 ## Unregistered Issue PYTHON-DESTRUCTURING-WORSE-THAN-USELESS-DUE-TO-NEEDLESS-COERCION
-                (name, pat0), patR = maybe_destructure_binding(pat[0]), pat[1:]
+                (name, pat0), patR = _maybe_destructure_binding(pat[0]), pat[1:]
                 return name, pat0, patR, (((pat0,) + patR) if name is not None else
                                                  pat) ## Attempt to avoid consing..
         ## I just caught myself feeling so comfortable thinking about life matters,
         ## while staring at a screenful of code.  In "real" life I'd be pressed by
         ## the acute sense of time being wasted..
         atomp, null = atom(pat), pat == ()
-        # dprint("       _match  %20s  %10s  %s  %s  %s  %s  %s", bound, name, exp, pat, leader, aux, limit)
+        # _debug_printf("       _match  %20s  %10s  %s  %s  %s  %s  %s", bound, name, exp, pat, leader, aux, limit)
         return \
             (test((match_atom(exp, pat) if atomp else
                    exp == ()),
@@ -175,87 +180,89 @@ def _match(bound, name, exp, pat, leader, aux, limit):
              (*maybe_get0Rname(pat)))
 
 def match(exp, pat):
-        name, prepped = maybe_destructure_binding(preprocess(pat))
+        name, prepped = _maybe_destructure_binding(preprocess(pat))
         return _match(dict(), name, exp, prepped, True, None, None)
 
 print("\n; compiled and loaded.")
 ###
 ### app
 ###
-newline        = "newline"
-indent         = "indent"
-form           = "form"
-count_scope    = "count_scope"
-def forc(x, y, leader):
-        return (("OR",) if leader else ()) + (x,) + y
+_pp_base_depth = 0
+_pp_depth      = 0
+_newline        = "newline"
+_indent         = "indent"
+_form           = "form"
+_count_scope    = "count_scope"
 def preprocess(pat):
         "Expand syntactic sugar."
         def prep_binding(b):
-                k, v = tuple(b.items())[0]
+                k, v = _py.tuple(b.items())[0]
                 return {k: preprocess(v)}
-        return ((count_scope, (some,) +
-                 preprocess(tuple(pat)))           if isinstance(pat, list)                else
-                prep_binding(pat)                  if isinstance(pat, dict)                else
-                (form,)                            if pat == form                          else
-                (newline, 0)                       if pat == "\n"                          else
-                (newline, pat)                     if integerp(pat)                        else
-                (indent, 1)                        if pat == " "                           else
-                pat                                if not (pat and isinstance(pat, tuple)) else
+        return ((_count_scope, (some,) +
+                 preprocess(_py.tuple(pat)))       if typep(pat, _py.list)       else
+                prep_binding(pat)                  if typep(pat, _py.dict)       else
+                (_form,)                           if pat == _form               else
+                (_newline, 0)                      if pat == "\n"                else
+                (_newline, pat)                    if integerp(pat)              else
+                (_indent, 1)                       if pat == " "                 else
+                pat                                if not (pat and _tuplep(pat)) else
                 (preprocess(pat[0]),) + preprocess(pat[1:]))
 def nonliteral_atom_p(x):
         return x == "name"
 def match_atom(exp, pat):
-        return isinstance(exp, int)
-pp_base_depth = 0
-pp_depth = 0
+        return integerp(exp)
 def prod(x, leader):
-        return str(x) if x or leader else ""
+        return _py.str(x) if x or leader else ""
 def comb(f0, fR, leader):
-        global pp_base_depth, pp_depth
+        global _pp_base_depth, _pp_depth
         acc = "(" if leader else ""
-        base_depth_save = pp_base_depth
+        base_depth_save = _pp_base_depth
         try:
                 if leader:
-                        pp_base_depth = pp_base_depth + pp_depth + 1
+                        _pp_base_depth = _pp_base_depth + _pp_depth + 1
                 res0 = f0()
                 if res0 is None: return
                 acc += res0
                 try:
-                        pp_depth += len(res0)
+                        _pp_depth += length(res0)
                         resR = fR()
                         if resR is None: return
                         acc += resR
                         acc += ")" if leader else ""
                 finally:
-                        pp_depth -= len(res0)
+                        _pp_depth -= len(res0)
         finally:
-                pp_base_depth = base_depth_save
+                _pp_base_depth = base_depth_save
         return acc
 def process_newline(bound, name, exp, pat, leader, aux):
-        global pp_base_depth, pp_depth
+        global _pp_base_depth, _pp_depth
         n, tail = pat[0][1], pat[1:]
         try:
-                pp_base_depth += n
-                pp_depth = 0
-                return post(_match(bind(pp_base_depth, bound, name), None, exp, tail, leader, aux, None),
-                            lambda r: "\n" + (" " * pp_base_depth) + r)
+                _pp_base_depth += n
+                _pp_depth = 0
+                return post(_match(bind(_pp_base_depth, bound, name), None, exp, tail, leader, aux, None),
+                            lambda r: "\n" + (" " * _pp_base_depth) + r)
         finally:
-                pp_base_depth -= n
+                _pp_base_depth -= n
 def process_indent(bound, name, exp, pat, leader, aux):
-        global pp_base_depth, pp_depth
+        global _pp_base_depth, _pp_depth
         n, tail = pat[0][1], pat[1:]
         try:
-                pp_depth += n
-                return post(_match(bind(pp_depth, bound, name), None, exp, tail, leader, aux, None),
+                _pp_depth += n
+                return post(_match(bind(_pp_depth, bound, name), None, exp, tail, leader, aux, None),
                             lambda r: (" " * n) + r)
         finally:
-                pp_depth -= n
+                _pp_depth -= n
+def _metasex_matcher_init():
+        _matcher_init()
+        register_complex_matcher(_newline,     process_newline)
+        register_complex_matcher(_indent,      process_indent)
+        register_complex_matcher(_form,        matcher_not_implemented)
+        register_complex_matcher(_count_scope, identity_matcher)
+def forc(x, y, leader):
+        return (("OR",) if leader else ()) + (x,) + y
 
-register_complex_matcher(newline,     process_newline)
-register_complex_matcher(indent,      process_indent)
-register_complex_matcher(form,        matcher_not_implemented)
-register_complex_matcher(count_scope, identity_matcher)
-
+_metasex_matcher_init()
 ###
 ### testing
 ###
@@ -268,8 +275,8 @@ def runtest(fun, bindings, result):
                 f is None)
 def results():
         for fun, b, r, f in _results:
-                dprint("%15s bound: %s", fun.__name__, b)
-                dprint("%15s res: %s", fun.__name__, r)
+                _debug_printf("%15s bound: %s", fun.__name__, b)
+                _debug_printf("%15s res: %s", fun.__name__, r)
 def empty():
         return match((), {"whole":()})
 bound_good, result_good, nofail = runtest(empty,
@@ -281,7 +288,7 @@ assert(result_good)
 print("; EMPTY: passed")
 
 def empty_cross():
-        return match((), ({"a":[name]}, {"b":[(name,)]},))
+        return match((), ({"a":[_name]}, {"b":[(_name,)]},))
 bound_good, result_good, nofail = runtest(empty_cross,
                                           { 'a': (), 'b': () },
                                           "()")
@@ -291,13 +298,13 @@ assert(result_good)
 print("; EMPTY-CROSS: passed")
 
 def mid_complex():
-        pat = ({"headname":name},
-                  {"headtupname":(name,)},
-                           {"varitupseq":[(name, [name])]},
-                                                    {"fix1tupseq":[(name,)]},
-                                                                           {"nameseq":[name]},
-                                                                                {"tailname":name})
-        exp =             (1,    (1,),   (1,), (1, 1), (1, 1, 1), (1,), (1,), (1,),   1, 1, 1)
+        pat = ({"headname":_name},
+                  {"headtupname":(_name,)},
+                           {"varitupseq":[(_name, [_name])]},
+                                                    {"fix1tupseq":[(_name,)]},
+                                                                           {"nameseq":[_name]},
+                                                                                {"tailname":_name})
+        exp =              (1,   (1,),   (1,), (1, 1), (1, 1, 1), (1,), (1,), (1,),   1, 1, 1)
         return match(exp, pat)
 bound_good, result_good, nofail = runtest(mid_complex,
                                           { 'headname': 1,
@@ -315,7 +322,7 @@ assert(result_good)
 print("; MID-COMPLEX: passed")
 
 def simple_maybe():
-        return match((1, 2, 3), ({"a":(maybe, name)}, {"b":name}, (maybe, {"c":name})))
+        return match((1, 2, 3), ({"a":(_maybe, _name)}, {"b":_name}, (_maybe, {"c":_name})))
 bound_good, result_good, nofail = runtest(simple_maybe,
                                           { 'a': (1,), 'b': 2, 'c': 3, },
                                           "(123)")
