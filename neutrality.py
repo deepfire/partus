a0faf501cc33e0235968fb6d74a62086191e71c1
Samium Gromoff <_deepfire@feelingofgreen.ru>
2012-12-04 21:00:38 +0400

    neutrality: means to query python3 version

--- neutrality.py
+++ neutrality.py
@@ -7,6 +7,8 @@ py_ver_minor = platform.python_version_tuple()[1]
 def py2p(): return py_ver_major == '2'
 def py3p(): return py_ver_major == '3'
 
+def py3minor_atleast(x): return int(py_ver_minor) >= x
+
 if py2p():
         from neutrality_py2 import stringp, do_write_string, file_content, set_file_content, to_unicode, to_raw_text, hunter_seeker, package_hunter_seeker
 elif py3p():
@@ -17,6 +19,7 @@ do_write_string("python neutrality: initialised for version %s\n" % py_ver_major
 
 __all__ = ['py2p',
            'py3p',
+           'py3atleastp',
            ###
            'stringp',
            'do_write_string',
