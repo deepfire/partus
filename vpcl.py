4ce97d4b373ef7797c50d0b7e8cdbb3ce97a85d2
Samium Gromoff <_deepfire@feelingofgreen.ru>
2013-01-09 01:41:32 +0400

    vpcl.py: a top-level loadable, intended to serve as the global entry point

---
+++ vpcl.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import cl
+from cl import *
+
+version           = "0.0.1"
+vpcl_install_root = "/home/deepfire/src/partus/"
+sicl_source_root  = "/home/deepfire/src/sicl/Code/"
+
+def VPCL(file):
+        return vpcl_install_root + file
+
+def SICL(file):
+        return sicl_source_root + file
+
+def lcf(locator, file):
+        def pseudo_compile_file_pathname(x):
+                pieces = x.split(".")
+                assert pieces[-1] in {"lisp", "lsp"}
+                return ".".join(pieces[:-1]) + '.vpfas'
+        compiled_file = pseudo_compile_file_pathname(file)
+        if not probe_file(locator(compiled_file)) or file_write_date(locator(file)) > file_write_date(locator(compiled_file)):
+                compile_file(locator(file))
+        load(locator(compiled_file))
+
+# print("This is VPCL %s, a piece of code mockingly approaching to an implementation of ANSI Common Lisp.\n" % version)
+
+lcf(VPCL, "vpcl.lisp")
+
+# lcf(SICL, "Reader/float.lisp")
+# lcf(SICL, "Reader/read.lisp")
+
+dprintf("; *PACKAGE* is %s", symbol_value(cl._package_))
+
+repl()
