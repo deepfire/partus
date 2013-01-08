2470d8a3c891d4ede47fa4d975be5941705106eb
Samium Gromoff <_deepfire@feelingofgreen.ru>
2013-01-09 01:37:36 +0400

    neutrality: be silent

--- neutrality.py
+++ neutrality.py
@@ -15,7 +15,7 @@ elif py3p():
         from neutrality_py3 import stringp, do_write_string, file_content, set_file_content, to_unicode, to_raw_text, hunter_seeker, package_hunter_seeker
         pass
 
-do_write_string("python neutrality: initialised for version %s\n" % py_ver_major, sys.stdout)
+# do_write_string("python neutrality: initialised for version %s.%s\n" % (py_ver_major, py_ver_minor), sys.stdout)
 
 __all__ = ['py2p',
            'py3p',
